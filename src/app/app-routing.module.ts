4711e5a714804a7359accfe5109c2f5ae15f69a0
elizaveta.kornilo <elizaveta.kornilo@intexsoft.by>
2022-09-06 09:57:21 +0300

    add crisis-list page, add styles to active links
    ITX5OTHER-708-Обучение-Angular-Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import {DashboardComponent} from "./dashboard/dashboard.component";
 import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {HeroResolverService} from "./hero-resolver.service";
+import {CrisisListComponent} from "./crisis-list/crisis-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent, resolve: { hero: HeroResolverService } },
   { path: 'heroes', component: HeroesComponent },
-  {path: '404', component: NotFoundComponent},
-  {path: '**', redirectTo: '/404'}
+  { path: 'crisis-list', component: CrisisListComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     MessagesComponent,
     DashboardComponent,
     NotFoundComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
