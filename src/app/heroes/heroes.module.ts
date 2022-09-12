be615db8291bf263d7bacfeaa01263f9caa71c8c
elizaveta.kornilo <elizaveta.kornilo@intexsoft.by>
2022-09-12 10:01:02 +0300

    make the add hero form reactive, use a material in it, move it to a separate component
    ITX5OTHER-709-ОбучениеAngular-Form

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HeroesRoutingModule } from './heroes-routing.module';
+
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+
 import {HeroListComponent} from "./hero-list/hero-list.component";
 import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 import {HeroSearchComponent} from "./hero-search/hero-search.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
-import {FormsModule} from "@angular/forms";
-
+import { HeroAddFormReactiveComponent } from './hero-add-form-reactive/hero-add-form-reactive.component';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -15,11 +20,18 @@ import {FormsModule} from "@angular/forms";
     HeroDetailComponent,
     HeroSearchComponent,
     DashboardComponent,
+    HeroAddFormReactiveComponent,
   ],
   imports: [
     CommonModule,
     HeroesRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    // material:
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class HeroesModule { }
